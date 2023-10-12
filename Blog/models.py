d6e61a9a1949f2dbb11fbfa0a7a0db99e4167bfd
GreenMonkii <abramsharry6@gmail.com>
2023-10-12 16:24:24 +0100

    Updates to models and index.html

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime as dt
+from django.core.validators import RegexValidator
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=False)
@@ -12,10 +14,27 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+
+class Writer(models.Model):
+    first_name = models.CharField(
+        max_length=255, blank=False, null=False, verbose_name="first name")
+    last_name = models.CharField(
+        max_length=255, blank=False, null=False, verbose_name="last name")
+    username = models.CharField(max_length=255, blank=False, unique=True, null=False,
+                                help_text="Your username should only contain alphabets, numbers and underscores (-)",
+                                validators=[RegexValidator(r"^[\w\d-]{5,}")])
+    join_date = models.DateTimeField(auto_now=True)
+    display_picture = models.ImageField(null=True, upload_to="user-img", blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=1024, blank=False)
     post = models.TextField(blank=False)
     slug = models.SlugField(unique=True)
+    writer = models.ForeignKey(to=Writer, on_delete=models.PROTECT, null=True, related_name="Posts")
     image_url = models.URLField(blank=True)
     creation_date = models.DateTimeField(auto_now=True)
     modified_date = models.DateTimeField(auto_now=True)
